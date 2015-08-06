ce2b150f615a51df79c5f329de523d2ba2c80bfd
Takuya.Okamoto <blackn.red42@gmail.com>
2015-08-06 10:50:46 +0900

    OK

---
+++ TKAnimatedCheckButton/Classes/TKAnimatedCheckButton.swift
@@ -0,0 +1,130 @@
+//
+//  TKAnimatedCheckButton.swift
+//  TKAnimatedCheckButton
+//
+//  Created by Takuya Okamoto on 2015/08/06.
+//  Copyright (c) 2015年 Uniface. All rights reserved.
+
+// Inspired by
+//http://robb.is/working-on/a-hamburger-button-transition/
+//https://dribbble.com/shots/1631598-On-Off
+
+
+import Foundation
+import CoreGraphics
+import QuartzCore
+import UIKit
+
+
+class TKAnimatedCheckButton : UIButton {
+    
+    var shape: CAShapeLayer! = CAShapeLayer()
+    
+    let path: CGPath = {
+        let p = CGPathCreateMutable()
+        CGPathMoveToPoint(p, nil, 5.07473346,20.2956615)
+        CGPathAddCurveToPoint(p, nil, 3.1031115,24.4497281, 2,29.0960413, 2,34)
+        CGPathAddCurveToPoint(p, nil, 2,51.673112, 16.326888,66, 34,66)
+        CGPathAddCurveToPoint(p, nil, 51.673112,66, 66,51.673112, 66,34)
+        CGPathAddCurveToPoint(p, nil, 66,16.326888, 51.673112,2, 34,2)
+        CGPathAddCurveToPoint(p, nil, 21.3077047,2, 10.3412842,9.38934836, 5.16807419,20.1007094)
+        CGPathAddLineToPoint(p, nil, 29.9939289,43.1625671)
+        CGPathAddLineToPoint(p, nil, 56.7161293,17.3530369)
+        return p
+        }()
+    
+    let circleStrokeStart: CGFloat = 0.0//0.028
+    let circleStrokeEnd: CGFloat = 0.738//0.111
+    
+    let checkStrokeStart: CGFloat = 0.8
+    let checkStrokeEnd: CGFloat = 0.97
+    
+    let opacity:CGFloat = 0.4//0.15が近い
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.shape.path = path
+        let circle = CAShapeLayer()
+        circle.path = path
+        let check = CAShapeLayer()
+        check.path = path
+        
+        self.shape.strokeColor = UIColor.whiteColor().CGColor
+        circle.strokeColor = UIColor.whiteColor().colorWithAlphaComponent(opacity).CGColor
+        check.strokeColor = circle.strokeColor
+        
+        for layer in [ self.shape, circle, check] {
+            layer.fillColor = nil
+            layer.lineWidth = 4
+            layer.miterLimit = 4
+            layer.lineCap = kCALineCapRound
+            layer.masksToBounds = true
+            // boundsをとるためにコピー？
+            let strokingPath:CGPath = CGPathCreateCopyByStrokingPath(layer.path, nil, 4, kCGLineCapRound, kCGLineJoinMiter, 4)
+            layer.bounds = CGPathGetPathBoundingBox(strokingPath)
+            layer.actions = [
+                "strokeStart": NSNull(),
+                "strokeEnd": NSNull(),
+                "transform": NSNull()
+            ]
+            self.layer.addSublayer(layer)
+        }
+        
+        self.shape.position = CGPointMake(27, 27)
+        circle.position = self.shape.position
+        check.position = self.shape.position
+        
+        self.shape.strokeStart = circleStrokeStart
+        self.shape.strokeEnd = circleStrokeEnd
+        circle.strokeStart = circleStrokeStart
+        circle.strokeEnd = circleStrokeEnd
+        check.strokeStart = checkStrokeStart
+        check.strokeEnd = checkStrokeEnd
+    }
+    
+    var checked: Bool = false {
+        didSet {
+            let timingFunc = CAMediaTimingFunction(controlPoints: 0.69,0.12,0.23,1.27)
+            // マルのPath上の描画地点をアニメーションさせる  keyPathはアニメーションするプロパティ
+            let strokeStart = CABasicAnimation(keyPath: "strokeStart")
+            let strokeEnd = CABasicAnimation(keyPath: "strokeEnd")
+            if self.checked {
+                strokeStart.toValue = checkStrokeStart
+                strokeStart.duration = 0.5
+                strokeStart.timingFunction = timingFunc
+                
+                strokeEnd.toValue = checkStrokeEnd
+                strokeEnd.duration = 0.6
+                strokeEnd.timingFunction = timingFunc
+            } else {
+                strokeStart.toValue = circleStrokeStart
+                strokeStart.duration = 0.5
+                strokeStart.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, 0, 0.5, 1.2)
+                strokeStart.beginTime = CACurrentMediaTime() + 0.1
+                strokeStart.fillMode = kCAFillModeBackwards
+                
+                strokeEnd.toValue = circleStrokeEnd
+                strokeEnd.duration = 0.6
+                strokeEnd.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, 0.3, 0.5, 0.9)
+            }
+            self.shape.ocb_applyAnimation(strokeStart)
+            self.shape.ocb_applyAnimation(strokeEnd)
+        }
+    }
+}
+
+extension CALayer {
+    func ocb_applyAnimation(animation: CABasicAnimation) {
+        let copy = animation.copy() as! CABasicAnimation
+        if copy.fromValue == nil {
+            copy.fromValue = self.presentationLayer().valueForKeyPath(copy.keyPath)
+        }
+        self.addAnimation(copy, forKey: copy.keyPath)
+        self.setValue(copy.toValue, forKeyPath:copy.keyPath)
+    }
+}
--- TKAnimatedCheckButton/ViewController.swift
+++ TKAnimatedCheckButton/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var button: TKAnimatedCheckButton! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(red: 0.176471, green: 0.701961, blue: 0.203922, alpha: 1)
+        
+        self.button = TKAnimatedCheckButton(frame: CGRectMake(133, 133, 54, 54))
+        self.button.addTarget(self, action: "toggle:", forControlEvents:.TouchUpInside)
+        self.view.addSubview(button)
+    }
+    func toggle(sender: AnyObject!) {
+        self.button.checked = !self.button.checked
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func preferredStatusBarStyle() -> UIStatusBarStyle  {
+        return .LightContent
     }
-
-
 }
-
